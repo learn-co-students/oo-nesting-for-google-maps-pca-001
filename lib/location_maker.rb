a273c2624312fbd10a3d41ecf520d7cf4b2040ec
Stephen McKeon <stephen.mckeon@powerhrg.com>
2020-05-08 22:51:52 -0400

    Done.

---
+++ lib/location_maker.rb
@@ -0,0 +1,11 @@
+class LocationMaker
+  @@google_maps_array = []
+
+  def self.add_location_to_array(location_name, latitude, longitude)
+    @@google_maps_array << [location_name, latitude, longitude]
+  end
+
+  def self.map_locations()
+    @@google_maps_array
+  end
+end
